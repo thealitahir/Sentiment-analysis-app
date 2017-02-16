c8596e6c52bdfab0386ad4d68124f017b80779a3
maria <maria.zahid@platalytics.com>
2017-02-16 13:47:46 +0500

    url changed

--- testConfig.js
+++ testConfig.js
@@ -3,14 +3,11 @@ module.exports = {
         id : '54f81d07f7da9fde14330cb9'
     },
     database:{
-        ip:'122.129.79.68',
+        ip:'mongo-db-svc.iot-platform-sku-group-0',
         //ip:'54.164.145.190',
-        db:'test',
-        port: 9876
+        db:'test'
     },
     pipelineId:'5811ef825b2686b20192a382',
-     //smartSinkQueryUrl:'http://52.91.170.12:5000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/5811ef820c14f6c2480e7839/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT,Tweet_Id,%20userName,%20screenName,location,dateTime%20,status,%20HashTags%20from%20TABLE5811EF820C14F6C2480E7839%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545'
-    //smartSinkQueryUrl:'http://54.144.214.71:5000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/5811ef820c14f6c2480e7839/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT,Tweet_Id,%20userName,%20screenName,location,dateTime%20,status,%20HashTags%20from%20TABLE5811EF820C14F6C2480E7839%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545'
-    smartSinkQueryUrl:"http://122.129.79.68:3000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/5880c53da45e641e50d53e58/?query=select Predicted_label_sentiment as PREDICTED_SENTIMENT,Tweet_Id, userName, screenName,location,dateTime ,status, HashTags from TABLE5880C53DA45E641E50D53E58 where HashTags is not null and HashTags <> 'null' order by TWEET_ID desc limit 1000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545"
-};
-
+    //smartSinkQueryUrl:'http://52.91.170.12:5000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/5811ef820c14f6c2480e7839/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT,Tweet_Id,%20userName,%20screenName,location,dateTime%20,status,%20HashTags%20from%20TABLE5811EF820C14F6C2480E7839%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545'
+    smartSinkQueryUrl:'http://24.16.42.120:5000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/58a4650c6ad364cbcc108bd8/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT,Tweet_Id,%20userName,%20screenName,location,dateTime%20,status,%20HashTags%20from%20TABLE58A4650C6AD364CBCC108BD8%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545'
+}
