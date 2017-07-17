f8b8937a1d2189302e592f159bd391487b8ac786
asma <asma.sardar@platalytics.com>
2017-07-17 20:34:35 +0500

    Sink query updations

--- routes/index.js
+++ routes/index.js
@@ -69,7 +69,7 @@ router.get('/', function (req, res, next) {
 
 });
 router.get('/getApiUrl', function(req, res, next) {
-    var url = getConf().baseUrl + "/platalytics/api/version/developers_interface/process/"+CONFIGURATIONS.processId+"/smart_sink/"+CONFIGURATIONS.smartSinkId+"/?query=select%20*%20from%20TABLE"+CONFIGURATIONS.smartSinkId.toUpperCase()+"&sink_type=smart&start=0&rows=500&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b";
+    var url = getConf().baseUrl + "/platalytics/api/version/developers_interface/process/"+CONFIGURATIONS.processId+"/smart_sink/"+CONFIGURATIONS.smartSinkId+"/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT%2CTweet_Id%2C%20userName%2C%20screenName%2Clocation%2CdateTime%20%2Cstatus%2C%20HashTags%20from%20TABLE"+CONFIGURATIONS.smartSinkId.toUpperCase()+"%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545";
     res.send({status:true , data:url});
 });
 router.get('/fetchData',function(req, res,next){
@@ -266,7 +266,7 @@ function getConf(){
         baseUrl: "http://" + CONFIGURATIONS.frontEndHost + ':' + CONFIGURATIONS.frontEndPort,
         pipelineId: CONFIGURATIONS.processId,
         //smartSinkQueryUrl:'http://24.16.42.120:5000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/58a4650c6ad364cbcc108bd8/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT,Tweet_Id,%20userName,%20screenName,location,dateTime%20,status,%20HashTags%20from%20TABLE58A4650C6AD364CBCC108BD8%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545'
-        smartSinkQueryUrl:'http://'+CONFIGURATIONS.backEndHost+':'+CONFIGURATIONS.backEndPort+'/services/api/querysink/getData?process='+CONFIGURATIONS.processId+'%3A'+CONFIGURATIONS.smartSinkId+'%3B&query=select%20*%20from%20TABLE'+CONFIGURATIONS.smartSinkId.toUpperCase()+'&sink_type=smart&start=0&rows=500&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b',
+        smartSinkQueryUrl:'http://'+CONFIGURATIONS.backEndHost+':'+CONFIGURATIONS.backEndPort+'/services/api/querysink/getData?process='+CONFIGURATIONS.processId+'%3A'+CONFIGURATIONS.smartSinkId+'%3B&query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT%2CTweet_Id%2C%20userName%2C%20screenName%2Clocation%2CdateTime%20%2Cstatus%2C%20HashTags%20from%20TABLE'+CONFIGURATIONS.smartSinkId.toUpperCase()+'%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_type=smart&start=0&rows=500&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b',
     };
     //console.log(conf);
     return conf;
