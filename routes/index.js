5c3ae1f634b0fdba1b1bfeac5bf51a589e6046b3
ALI-TAHIR-LT-PC\ali <thealitahir431@gmail.com>
2019-01-30 15:57:44 +0500

    process id and sink id changed

--- routes/index.js
+++ routes/index.js
@@ -303,7 +303,7 @@ function getConf(){
         //smartSinkQueryUrl:'http://24.16.42.120:5000/platalytics/api/version/developers_interface/process/5811ef825b2686b20192a382/smart_sink/58a4650c6ad364cbcc108bd8/?query=select%20Predicted_label_sentiment%20as%20PREDICTED_SENTIMENT,Tweet_Id,%20userName,%20screenName,location,dateTime%20,status,%20HashTags%20from%20TABLE58A4650C6AD364CBCC108BD8%20where%20HashTags%20is%20not%20null%20and%20HashTags%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000&sink_profile=5804911c87b25b0e5b29653e&cluster_profile=5804911a87b25b0e5b29653b&start=0&rows=500&api_key=35454545'
         smartSinkQueryUrl:'http://'+CONFIGURATIONS.backEndHost+':'+
         CONFIGURATIONS.backEndPort+'/services/api/smartsink/'+CONFIGURATIONS.smartSinkId+
-        '?rows=10000&query=select%20PREDICTED_LABEL%20as%20PREDICTED_SENTIMENT%2CTWEET_ID%2C%20USERNAME%2C%20SCREENNAME%2CDATETIME%20%2CSTATUS%2C%20HASHTAGS%20from%20TABLE5C2F5C5F84987A673AF6336E%20where%20HASHTAGS%20is%20not%20null%20and%20HASHTAGS%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000'
+        '?rows=10000&query=select%20PREDICTED_LABEL%20as%20PREDICTED_SENTIMENT%2CTWEET_ID%2C%20USERNAME%2C%20SCREENNAME%2CDATETIME%20%2CSTATUS%2C%20HASHTAGS%20from%20TABLE'+CONFIGURATIONS.smartSinkId+'%20where%20HASHTAGS%20is%20not%20null%20and%20HASHTAGS%20%3C%3E%20%27null%27%20order%20by%20TWEET_ID%20desc%20limit%201000'
 
         /*baseUrl: "http://" + CONFIGURATIONS.frontEndHost + ':' + CONFIGURATIONS.frontEndPort,
         pipelineId: CONFIGURATIONS.processId,
